804b0e05f498caf4de3f167cd947f6342302159b
GKnibbs <georgeknibbs16@hotmail.com>
2025-07-22 11:47:07 +0100

    Fixing Null Error
    When the Page first loads, the count in "count" is null, must wait for DOM event

--- scripts/ViewCountingScript.js
+++ scripts/ViewCountingScript.js
@@ -9,15 +9,22 @@ const firebaseConfig = {
 };
 // Logging Raw Viewer Count -- NO IDENTIFYING INFO
 firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
-const counterRef = db.ref("views");
 
-// Counting Viewer
-counterRef.transaction(current => (current || 0) + 1);
-counterRef.on("value", snapshot => {
-    document.getElementById("count").innerText = snapshot.val();
+// Wrapping in a DOM Loading Event
+document.addEventListener("DOMContentLoaded", function () {
+    const db = firebase.database();
+    const counterRef = db.ref("views");
+
+    // Counting Viewer
+    counterRef.transaction(current => (current || 0) + 1);
+    counterRef.on("value", snapshot => {
+        document.getElementById("count").innerText = snapshot.val();
+        if (countEl) {
+        countEl.innerText = snapshot.val();
+        }
+    });
+
+    // Logging the Timestamp of Each Visit
+    const logsRef = db.ref("view_logs");
+    logsRef.push(new Date().toISOString());
 });
-
-// Logging the Timestamp of Each Visit
-const logsRef = db.ref("view_logs");
-logsRef.push(new Date().toISOString());
